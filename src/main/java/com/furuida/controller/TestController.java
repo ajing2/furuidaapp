a3a47afc986164de8f24ad5176c1ab9c6acdca76
lingjing <lingjing@jd.com>
2018-10-30 22:49:18 +0800

    购物车接口调用

--- src/main/java/com/furuida/controller/TestController.java
+++ src/main/java/com/furuida/controller/TestController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @program: furuidaapp
  * @description: contr
@@ -17,5 +20,18 @@ public class TestController {
     @ResponseBody
     public String getTest() {
         return "66666";
+    }
+
+
+
+    @RequestMapping("/ajax")
+    @ResponseBody
+    public HashMap<String, Object> getAjax(String action) {
+        HashMap<String, Object> data = new HashMap<>();
+        if (action.equals("addcart")){
+            data.put("error", 500);
+            data.put("message", "同意协议");
+        }
+        return data;
     }
 }
--- src/main/webapp/static/js/goods.js
+++ src/main/webapp/static/js/goods.js
@@ -53,20 +53,56 @@ function addToCart(goodsid, tt, pf) {
     goods.number = number;
     goods.price = prices;
     goods.optype = (typeof(tt) == 'undefined' || tt == "") ? "" : tt;
+
     createwindow();
     $.ajax({
         type: "POST",
-        url: SITE_URL + "ajax.php?action=addcart",
-        data: "goods=" + $.toJSON(goods),
-        dataType: "json",
+        url: "http://www.gflat.cn:8088/test/ajax?action=addcart",
         success: function (data) {
             removewindow();
             if (data.error == '500') {
-                $.post(SITE_URL + "product.php", {action: 'ajax_get_tongyidata1'}, function (data) {
-                    JqueryDialog.Open('确认服务商', data, 280, 200);
-                    $('.jd_dialog_m_t').hide();
-                    removewindow();
+                debugger;
+                $.ajax({
+                    url : "http://www.gflat.cn:8088/shopping/add",
+                    type : "POST",
+                    data : JSON.stringify({
+                        userId: '123321',
+                        goodsId: goods.goods_id,
+                        num: goods.number,
+                        createTime: 1,
+                        updateTime: 1
+                    }), //转JSON字符串
+                    dataType : 'json',
+                    contentType : 'application/json;charset=UTF-8', //contentType很重要
+                    success : function(result) {
+                        console.log(result);
+                        debugger;
+                    },
+                    error: function (result) {
+                        debugger;
+
+                    }
                 });
+
+                debugger;
+
+                result = "<div style=\"font-size:14px; line-height:26px; padding:10px; text-align:left;\">\n" +
+                    "    <p>您的推荐服务商是：</p>\n" +
+                    "    <p style=\"padding-left:30px; padding-bottom:10px; line-height:20px; height:20px;\"><b style=\"color:blue\">(ID:845632)王越仕</b></p>\n" +
+                    "    <p style=\"color:#F96\">*&nbsp;请确认购买产品数量，不要重复购买，购买后无法退单。</p>\n" +
+                    "    <p style=\"color:#F36\">*&nbsp;产品一经购买,除产品质量问题外,不支持无理由退货、退款！</p>\n" +
+                    "    <p style=\"color:#F00\">*&nbsp;产品购买前请确认服务商是否正确，一经购买下单后将无法更改上下级服务商信息，后果将个人承担。</p>\n" +
+                    "    <hr/>\n" +
+                    "    <p style=\"text-align:center; height:50px;\">\n" +
+                    "        <a onclick=\"return alertb();\" style=\" float:left;display:block; width:40%; line-height:35px;height:35px;border-radius:5px; background:#f35600; text-align:center; color:#fff\">服务商正确</a>\n" +
+                    "        <a onclick=\"return alerta();\" style=\" margin-left:20px;float:left;display:block; width:40%; line-height:35px; height:35px;border-radius:5px; background:#ff8f03; text-align:center; color:#fff\">服务商不对</a>\n" +
+                    "    </p>\n" +
+                    "</div>"
+
+                JqueryDialog.Open('确认服务商', result, 280, 200);
+                $('.jd_dialog_m_t').hide();
+                removewindow();
+
                 return false;
             }
 
@@ -197,6 +233,59 @@ function addToCart(goodsid, tt, pf) {
     return false;
 }
 
+
+
+function alerta(){
+    alert('请联系推荐服务商确认推荐关系!');
+    return false;
+}
+function alertb(){
+    createwindow();
+    // $.post(SITE_URL+"product.php",{action:'ajax_get_tongyidata'},function(data){
+    data = "<div style=\"font-size:14px; line-height:26px; padding:10px; text-align:left; height:400px; overflow:scroll;\">\n" +
+        "    <h2 style=\"text-align:center;\">商品购销协议</h2>\n" +
+        "    <p>甲方：美澳环保科技有限公司</p>\n" +
+        "    <p>乙方：消费者</p>\n" +
+        "    <p><span style=\"color:#e53333;\"><b>※购买前请确认个人中心服务商是否正确。</b></span></p>\n" +
+        "    <span style=\"color:#e53333;\"><b> </b></span><p><span style=\"color:#e53333;\"><b>※产品一经购买后，若无产品本身质量问题将无法退款、退货。</b></span></p>\n" +
+        "    <span style=\"color:#e53333;\"><b> </b></span><p><span style=\"color:#e53333;\"><b>备注⑴产品购买前请确认服务商是否正确，一经购买下单后将无法更改上下级服务商信息，后果将个人承担。</b></span></p>\n" +
+        "    <span style=\"color:#e53333;\"><b> </b></span><p><span style=\"color:#e53333;\"><b>⑵如果收货后发现有包装损坏、产品泄漏等外在因素，请参考（交货及验货、商品促销第3点）。</b></span></p>\n" +
+        "    <p>根据《中华人民共和国合同法》及其他有关法律、行政法规的规定，甲、乙双方遵循平等、自愿、公平和诚实信用的原则，就商品进货购销事宜协商订立本合同。</p>\n" +
+        "    <h2>一、订购商品</h2>\n" +
+        "    <p>1．上述商品价格已经双方确认，如因原材料价格、生产经营成本、市场供求关系等变化导致合同期内商品价格变化，要求价格变动一方应当提前15日通知对方。价格变动自确认之日起生效，适用于确认之日后的新订单。</p>\n" +
+        "    <p>2．甲方所提供商品的外包装应当符合中华人民共和国相关法律法规的规定，用中文标明产品名称、生产厂名与厂址、规格、等级、采用的产品标准、质量检验合格证明、使用说明、生产日期和安全使用期或者失效期、警示标志及其它说明等。商品应当使用正规条形码，以便于POS机识别。</p>\n" +
+        "    <p>3．甲方应当保证其所提供商品的质量符合本合同或订单约定的质量标准；甲方提供有关商品质量说明的，应当符合该说明的质量要求。质量要求不明确的，按照国家标准、行业标准履行；无国家标准、行业标准的，按照通常标准或者符合合同目的的特定标准履行。</p>\n" +
+        "    <h2>二、订货</h2>\n" +
+        "    <p>1．乙方向甲方订货，双方约定的订单形式为：（1）乙方电子商务平台（2）电子邮件（3）传真（4）订货合同（5）其他</p>\n" +
+        "    <p>2．订单应标明商品名称、生产厂名和厂址、规格、计量单位、品牌、质量、产地、数量、单价等具体内容。</p>\n" +
+        "    <h2>三、交货及验收</h2>\n" +
+        "    <p>1．甲方应当将订单列明的商品，按照约定的时间、运输方式交付到乙方指定地点。</p>\n" +
+        "    <p>2．商品的所有权自交付时起转移给乙方，商品毁损、灭失的风险自交付起由乙方承担。</p>\n" +
+        "    <p>3．乙方应当妥善安排工作人员或者本人在到货后当时内按照订单对商品的种类、规格、产地、数量、包装等进行初步验收。 一经验货完成，乙方不得无故退货。</p>\n" +
+        "    <p>4．乙方对于已经验收的商品发现存在内在质量问题，应当在质量保证期内提出。</p>\n" +
+        "    <h2>四、商品促销</h2>\n" +
+        "    <p>1．乙方可以根据企业经营战略制定商品促销计划，以加速商品的周转和销售。</p>\n" +
+        "    <p>2．甲方可以根据自身产品状况，有选择地参加促销活动，同时向乙方支付促销服务费用或者给予商品价格优惠。</p>\n" +
+        "    <p>3．乙方换货应当向甲方发出书面或电子信息换货通知，甲方应当于收到后5日内对所换商品进行核实并书面确认，10日内负责更换。</p>\n" +
+        "    <h2>五、争议的解决</h2>\n" +
+        "    <p>凡因执行本协议所发生的争议，或与本协议有关的一切争议，双方应通过友好协商解决。如果协商不成，可以向甲方住所地法院起诉。</p>\n" +
+        "    <div style=\"height:14px;\"></div>\n" +
+        "    <div style=\"text-align:center; position:absolute; bottom:0px; left:0px; z-index:999; width:100%; height:34px;\">\n" +
+        "        <a onclick=\"return tongyixieyi();\" style=\" display:block;border:none; height:34px; line-height:34px; background:#999; color:#fff; font-size:14px; text-align:center\">同意协议</a>\n" +
+        "    </div>\n" +
+        "</div>"
+        JqueryDialog.Open('商品购销协议',data,280,420);
+        $('.jd_dialog_m_t').hide();
+        removewindow();
+    // });
+}
+
+
+function tongyixieyi(){
+    JqueryDialog.Close();
+
+    return false;
+}
 //积分兑换
 function addToCartJifen(goodsid) {
 //判断js函数是否存在
