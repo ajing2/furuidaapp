ef91eedfeb709d3b972147dd6ddc69c916e4cc5a
lingjing <lingjing@jd.com>
2018-11-14 21:10:57 +0800

    修改登录页面

--- src/main/webapp/static/scripts/login.js
+++ src/main/webapp/static/scripts/login.js
@@ -3,6 +3,6 @@ var parent_id = window.location.search.replace("?parent_id=", "");
 localStorage.setItem("parentId", parent_id);
 
 
-var redirect_url = encodeURIComponent("/static/index.html");
+var redirect_url = encodeURIComponent("http://www.yitaonet.cn/static/index.html");
 var url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxcd28872d096b833d&redirect_uri=' + redirect_url + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
 window.location.href = url;
